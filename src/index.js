c16faf17338f8cc5a2afdecf855cc1d76cb0a46c
Adil Minocherhomjee <adilminocherhomjee@gmail.com>
2020-03-19 16:03:24 -0600

    updated src

--- src/index.js
+++ src/index.js
@@ -3,22 +3,26 @@ import '../assets/css/style.css';
 const app = document.getElementById('app');
 app.innerHTML = '<h1>JavaScript Basics</h1>';
 
-// truthy values
-console.log(!!true);
-console.log(Boolean(true));
-console.log(!!{});
-console.log(!![]);
-console.log(!!new Date());
-console.log(!!'0');
-console.log(!!42);
-console.log(!!-42);
-console.log(!!3.14);
-console.log(!!-3.14);
+// ----
+const condition = true;
 
-// falsy values
-console.log(!!false);
-console.log(!!null);
-console.log(!!undefined);
-console.log(!!0);
-console.log(!!NaN);
-console.log(!!'');
+if (condition) {
+  console.log('Yes');
+} else {
+  console.log('No');
+}
+
+const number = 99;
+let result; // undefined
+
+if (number === 1) {
+  result = 'One';
+} else if (number === 99) {
+  result = 'Ninety-Nine';
+} else if (number === 1000) {
+  result = 'One Thousand';
+} else {
+  result = 'No Match';
+}
+
+console.log(result);
