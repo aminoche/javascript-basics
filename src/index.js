a57d49004a6b1d67ed463285cb626c019e38e932
Adil Minocherhomjee <adilminocherhomjee@gmail.com>
2020-03-20 23:00:24 -0600

    playing with booleans

--- src/index.js
+++ src/index.js
@@ -4,21 +4,8 @@ const app = document.getElementById('app');
 app.innerHTML = '<h1>JavaScript Basics</h1>';
 
 // ----
-const flavor = 'yum';
-console.log(String.prototype);
-console.log('Pizza'.fontcolor('yellow'));
-console.log('Pizza is "good"!');
-console.log(`Pizza is "${flavor.toUpperCase()}"`);
-console.log(String(55 + 11), String(55 + '11'));
-console.log(String({ name: 'Pepperoni' }));
-console.log(String([1, 2, 3, 4]));
-console.log(new String(55).valueOf());
-
-const immutableString = 'I shall not change!';
-const upperCaseString = immutableString.toUpperCase();
-
-console.log(immutableString, upperCaseString);
-
-console.log(Object.prototype.toString.call('a'));
-
-console.log('abc'.padEnd(10, '-'));
+console.log(true, false);
+console.log(typeof false);
+console.log(true instanceof Boolean);
+console.log(new Boolean('X') instanceof Boolean);
+console.log(Object.prototype.toString.call(true).slice(8, -1));
