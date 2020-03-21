ec5169d6fec283c09facdf3bd80d25df30fe8a58
Adil Minocherhomjee <adilminocherhomjee@gmail.com>
2020-03-21 08:29:13 -0600

    function fun

--- src/index.js
+++ src/index.js
@@ -4,8 +4,30 @@ const app = document.getElementById('app');
 app.innerHTML = '<h1>JavaScript Basics</h1>';
 
 // ----
-console.log(true, false);
-console.log(typeof false);
-console.log(true instanceof Boolean);
-console.log(new Boolean('X') instanceof Boolean);
-console.log(Object.prototype.toString.call(true).slice(8, -1));
+
+console.log(makeCar);
+console.log(makeCarExpression);
+console.log(makeCarArrow);
+console.log(makeCarArrowShorthand);
+
+//1: Function declaration -- hoisted
+function makeCar() {
+  console.log('Making a car...');
+}
+
+makeCar();
+
+//2: Function expression (anonymous or named) -- not hoisted
+const makeCarExpression = function myFunction() {};
+console.log(makeCarExpression.name);
+
+//3: Arrow function
+
+const makeCarArrow = () => {
+  console.log('Making car inside Arrow...');
+};
+
+makeCarArrow();
+
+const makeCarArrowShorthand = () => console.log('Short');
+makeCarArrowShorthand();
